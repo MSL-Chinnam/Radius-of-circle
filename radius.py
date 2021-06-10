b8297224ed1fd3cdb37f9f9d710b6ff7248e5bb2
MSL-Chinnam <85691683+MSL-Chinnam@users.noreply.github.com>
2021-06-10 20:11:46 +0530

    Created radius.py
    This file takes the value from the user and then calculate the area of the circle and prints the value area on the screen.

---
+++ radius.py
@@ -0,0 +1,5 @@
+pi=3.14159265
+radius=float(input())
+print("Input the radius of the circle: ",radius)
+area=pi * radius * radius
+print("The area of the circle with radius %f ",radius," is: ",area)
